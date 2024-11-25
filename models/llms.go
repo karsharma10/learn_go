ea4d05024c4f240dacb6b5f2b2ca4b30cf91048a
Kartik Sharma <karsharma10@gmail.com>
2024-11-25 15:46:30 -0700

    added openai

--- models/llms.go
+++ models/llms.go
@@ -3,6 +3,7 @@ package models
 import (
 	"context"
 	"github.com/tmc/langchaingo/llms/ollama"
+	"github.com/tmc/langchaingo/llms/openai"
 )
 
 type LLM interface {
@@ -36,6 +37,42 @@ func (o *OllamaModel) GenerateEmbedding() (func(ctx context.Context, texts []str
 
 func (o *OllamaModel) GenerateFromPrompt() (func(ctx context.Context, text string) (string, error), error) {
 	llm, err := ollama.New(ollama.WithModel(o.model))
+	if err != nil {
+		return nil, err
+	}
+	generatedText := func(ctx context.Context, text string) (string, error) {
+		return llm.Call(ctx, text)
+	}
+	return generatedText, nil
+}
+
+type OpenAIModel struct {
+	model          string
+	embeddingModel string
+}
+
+var _ LLM = (*OpenAIModel)(nil)
+
+func NewOpenAI(model string, embeddingModel string) *OpenAIModel {
+	return &OpenAIModel{
+		model:          model,
+		embeddingModel: embeddingModel,
+	}
+}
+
+func (o *OpenAIModel) GenerateEmbedding() (func(ctx context.Context, texts []string) ([][]float32, error), error) {
+	llm, err := openai.New(openai.WithEmbeddingModel(o.embeddingModel))
+	if err != nil {
+		return nil, err
+	}
+	embedding := func(ctx context.Context, texts []string) ([][]float32, error) {
+		return llm.CreateEmbedding(ctx, texts)
+	}
+	return embedding, nil
+}
+
+func (o *OpenAIModel) GenerateFromPrompt() (func(ctx context.Context, text string) (string, error), error) {
+	llm, err := openai.New(openai.WithModel(o.model))
 	if err != nil {
 		return nil, err
 	}

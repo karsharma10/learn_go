bc649240ded654e5710ae73ab383b0dbd892c63c
Kartik Sharma <karsharma10@gmail.com>
2024-11-30 16:17:26 -0700

    closure db

--- config/config.go
+++ config/config.go
@@ -12,6 +12,11 @@ const (
 
 type Configs struct {
 	OpenAIKey string
+	Host      string
+	Port      string
+	User      string
+	Password  string
+	Dbname    string
 }
 
 type Config func(*Configs)
@@ -24,5 +29,19 @@ func WithOpenAI() Config {
 	key := os.Getenv(OpenAIKey)
 	return func(configs *Configs) {
 		configs.OpenAIKey = key
+	}
+}
+
+func WithDb() Config {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal(err)
+	}
+	return func(configs *Configs) {
+		configs.Host = os.Getenv("DB_HOST")
+		configs.Port = os.Getenv("DB_PORT")
+		configs.User = os.Getenv("DB_USER")
+		configs.Password = os.Getenv("DB_PASSWORD")
+		configs.Dbname = os.Getenv("DB_NAME")
 	}
 }
--- db/db.go
+++ db/db.go
@@ -1,1 +1,11 @@
 package db
+
+import "github.com/karsharma10/learn_go/config"
+
+func ConnectDb() error {
+	// Connect to the database
+	dbConfig := config.Configs{}
+	getConfig := config.WithDb()
+	getConfig(&dbConfig)
+	return nil
+}
